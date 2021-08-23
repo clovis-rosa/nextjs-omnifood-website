508378c3ec3780d485448ddc12aff1348ba8b9be
Clovis Rosa <35171486+clovis-rosa@users.noreply.github.com>
2021-08-23 18:03:37 +0100

    Add images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,27 @@
+import Link from 'next/link'
 import styled from 'styled-components'
 
 export default function Header() {
   return (
     <>
       <HeaderStyles>
-        <h3>Header here</h3>
+        <Link href="#" passHref>
+          <a>
+            <img src="/images/omnifood.logo.png" alt="Omnifood logo" />
+          </a>
+        </Link>
       </HeaderStyles>
     </>
   )
 }
 
 const HeaderStyles = styled.header`
-  font-size: 50px;
-  color: rgb(40, 40, 40);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fdf2e9;
+
+  /* Because we want header to be sticky later */
+  height: 9.6rem;
+  padding: 0 4.8rem;
 `
