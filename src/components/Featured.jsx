63967a912e8a13115f4229ada8c2ed59deb06e1e
Clovis Rosa <35171486+clovis-rosa@users.noreply.github.com>
2021-08-24 22:10:32 +0100

    Added featured section component

---
+++ src/components/Featured.jsx
@@ -0,0 +1,85 @@
+import styled from 'styled-components'
+import { heroDeliveredData } from '@/data/HeroDeliveredData'
+
+export default function Featured() {
+  return (
+    <>
+      <FeaturedSectionStyles>
+        <FeaturedContainer>
+          <h2>As featured in</h2>
+          <FeaturedLogos>
+            <img src="/images/logos/techcrunch.png" alt="Techcrunch logo" />
+            <img
+              src="/images/logos/business-insider.png"
+              alt="Business Insider logo"
+            />
+            <img
+              src="/images/logos/the-new-york-times.png"
+              alt="The New York Times logo"
+            />
+            <img src="/images/logos/forbes.png" alt="Forbes logo" />
+            <img src="/images/logos/usa-today.png" alt="USA Today logo" />
+          </FeaturedLogos>
+        </FeaturedContainer>
+      </FeaturedSectionStyles>
+    </>
+  )
+}
+
+const FeaturedSectionStyles = styled.section`
+  padding: 4.8rem 0 3.2rem 0;
+
+  @media (max-width: 34em) {
+    padding: 2.4rem 0 6.4rem 0;
+  }
+`
+
+const FeaturedContainer = styled.div`
+  max-width: 120rem;
+  padding: 0 3.2rem;
+  margin: 0 auto;
+
+  h2 {
+    font-size: 1.4rem;
+    text-transform: uppercase;
+    letter-spacing: 0.75px;
+    font-weight: 500;
+    text-align: center;
+    margin-bottom: 2.4rem;
+    color: #888;
+  }
+`
+
+const FeaturedLogos = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, auto);
+  gap: 5rem;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-width: 84em) {
+  }
+
+  @media (max-width: 75em) {
+    grid-template-columns: repeat(3, auto);
+  }
+
+  @media (max-width: 59em) {
+    grid-template-columns: repeat(3, auto);
+  }
+
+  @media (max-width: 59em) {
+    grid-template-columns: repeat(2, auto);
+  }
+
+  @media (max-width: 34em) {
+    grid-template-columns: repeat(1, auto);
+  }
+
+  img {
+    height: 3.2rem;
+    display: block;
+    filter: brightness(0);
+    opacity: 50%;
+  }
+`
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import Layout from '@/components/Layout'
 import Hero from '@/components/Hero'
+import Featured from '@/components/Featured'
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
+      <Featured />
     </Layout>
   )
 }
