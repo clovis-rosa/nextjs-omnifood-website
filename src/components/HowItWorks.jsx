eb5a6ee5e4ce7a7c19e873db07912de789d7be03
Clovis Rosa <35171486+clovis-rosa@users.noreply.github.com>
2021-08-30 18:37:21 +0100

    Working on abstraction for How component, finished css grid reverse

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,4 +1,6 @@
+import React from 'react'
 import styled from 'styled-components'
+import { HowItWorksData } from '@/data/HowItWorksData'
 
 export default function HowItWorks() {
   return (
@@ -9,6 +11,29 @@ export default function HowItWorks() {
           <HowItWorksH2>
             Your daily dose of health in 3 simple steps
           </HowItWorksH2>
+          <HowItWorksGrid>
+            {HowItWorksData.map(
+              ({
+                id,
+                step_number,
+                heading_tertiary,
+                step_description,
+                step_img,
+                step_img_alt,
+              }) => (
+                <React.Fragment key={id}>
+                  <StepTextBox>
+                    <p className="step-number">{step_number}</p>
+                    <h3 className="heading-tertiary">{heading_tertiary}</h3>
+                    <p className="step-description">{step_description}</p>
+                  </StepTextBox>
+                  <StepImgBox>
+                    <img src={`/images/app/${step_img}`} alt={step_img_alt} />
+                  </StepImgBox>
+                </React.Fragment>
+              )
+            )}
+          </HowItWorksGrid>
         </HowItWorksContainer>
       </HowItWorksSectionStyles>
     </>
@@ -49,5 +74,98 @@ const HowItWorksH2 = styled.h2`
 
   @media (max-width: 75em) {
     font-size: 3.6rem;
+  }
+`
+
+const HowItWorksGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 6.4rem;
+  row-gap: 9.6rem;
+  margin-bottom: 9.6rem;
+  align-items: center;
+
+  max-width: 120rem;
+  padding: 0 3.2rem;
+  margin: 0 auto;
+
+  @media (max-width: 34em) {
+    grid-template-columns: 1fr;
+    row-gap: 4.8rem;
+  }
+
+  @media (max-width: 75em) {
+    column-gap: 4.8rem;
+    row-gap: 6.4rem;
+  }
+
+  div:nth-of-type(1) {
+    order: 1;
+  }
+  div:nth-of-type(2) {
+    order: 2;
+  }
+  div:nth-of-type(3) {
+    order: 4;
+  }
+  div:nth-of-type(4) {
+    order: 3;
+  }
+  div:nth-of-type(5) {
+    order: 5;
+  }
+  div:nth-of-type(6) {
+    order: 6;
+  }
+`
+
+const StepTextBox = styled.div`
+  .step-number {
+    font-size: 8.6rem;
+    font-weight: 600;
+    color: #ddd;
+    margin-bottom: 1.2rem;
+  }
+
+  .heading-tertiary {
+    font-size: 3rem;
+    line-height: 1.2;
+    margin-bottom: 3.2rem;
+  }
+
+  .step-description {
+    font-size: 1.8rem;
+    line-height: 1.8;
+  }
+`
+
+const StepImgBox = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &::before {
+    width: 60%;
+    height: 60%;
+    padding-bottom: 60%;
+    background-color: #fdf2e9;
+    z-index: -2;
+  }
+
+  &::before,
+  &::after {
+    content: '';
+    display: block;
+    border-radius: 50%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  img {
+    width: 35%;
+    /* z-index: 10; */
   }
 `
---
+++ src/data/HowItWorksData.js
@@ -0,0 +1,27 @@
+export const HowItWorksData = [
+  {
+    id: '001',
+    step_number: '01',
+    heading_tertiary: 'Tell us what you like (and what not)',
+    step_description:
+      'Never again waste time thinking about what to eat! Omnifood AI will create a 100% personalized weekly meal plan just for you. It makes sure you get all the nutrients and vitamins you need, no matter what diet you follow!',
+    step_img: 'app-screen-1.png',
+    step_img_alt: 'iPhone app meal approving plan screen',
+  },
+  {
+    id: '002',
+    step_number: '01',
+    heading_tertiary: 'Tell us what you like (and what not)',
+    step_description:
+      'Never again waste time thinking about what to eat! Omnifood AI will create a 100% personalized weekly meal plan just for you. It makes sure you get all the nutrients and vitamins you need, no matter what diet you follow!',
+    step_img: 'app-screen-1.png',
+  },
+  {
+    id: '003',
+    step_number: '01',
+    heading_tertiary: 'Tell us what you like (and what not)',
+    step_description:
+      'Never again waste time thinking about what to eat! Omnifood AI will create a 100% personalized weekly meal plan just for you. It makes sure you get all the nutrients and vitamins you need, no matter what diet you follow!',
+    step_img: 'app-screen-1.png',
+  },
+]
