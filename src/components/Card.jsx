29305d6b0faf8399f73ff16e8bf4479fabe28d91
Clovis Rosa <35171486+clovis-rosa@users.noreply.github.com>
2021-08-31 23:17:58 +0100

    Finished Card component with id iteration to display correct tags.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,50 +1,80 @@
 import React from 'react'
 import styled from 'styled-components'
 import { IconFlame, IconStar, IconRestaurant } from '@/components/SvgFiles'
+import { CardData } from '@/data/CardData'
 
 export default function Card() {
   return (
     <>
-      <CardStyles className="meal">
-        <img
-          className="meal-img"
-          src="images/meals/meal-1.jpg"
-          alt="Japanese Gyozas"
-        />
-        <div className="meal_content">
-          <div className="meal_tags">
-            <span className="tag tag_vegetarian">Vegetarian</span>
-          </div>
-          <p className="meal_title">Japanese Gyozas</p>
-          <ul className="meal_attributes">
-            <li className="meal_attribute">
-              <div>
-                <IconFlame />
+      {CardData.map(
+        ({
+          id,
+          img_src,
+          meal_tag_01,
+          meal_tag_02,
+          meal_tag_03,
+          meal_title,
+          icon_inner_01,
+          icon_inner_02,
+          icon_inner_03,
+          icon_inner_strong_01,
+          icon_inner_strong_02,
+          icon_inner_strong_03,
+        }) => (
+          <CardStyles className="meal" key={id}>
+            <img
+              className="meal-img"
+              src={`images/meals/${img_src}`}
+              alt={meal_title}
+            />
+            <div className="meal_content">
+              <div className="meal_tags">
+                <span
+                  className={`tag ${id === '002' ? 'none' : 'tag_vegetarian'}`}
+                >
+                  {meal_tag_01}
+                </span>
+                <span className={`tag ${id === '001' ? 'none' : 'tag_vegan'}`}>
+                  {meal_tag_02}
+                </span>
+                <span className={`tag ${id === '001' ? 'none' : 'tag_paleo'}`}>
+                  {meal_tag_03}
+                </span>
               </div>
-              <span>
-                <strong>650 </strong>calories
-              </span>
-            </li>
-            <li className="meal_attribute">
-              <div>
-                <IconRestaurant />
-              </div>
-              <span>
-                NutriScore ® <strong>74</strong>
-              </span>
-            </li>
-            <li className="meal_attribute">
-              <div>
-                <IconStar />
-              </div>
-              <span>
-                <strong>4.9 </strong>
-                rating (537)
-              </span>
-            </li>
-          </ul>
-        </div>
-      </CardStyles>
+              <p className="meal_title">{meal_title}</p>
+              <ul className="meal_attributes">
+                <li className="meal_attribute">
+                  <div className="icon_inner">
+                    <IconFlame />
+                  </div>
+                  <span>
+                    <strong>{icon_inner_strong_01}</strong>
+                    {icon_inner_01}
+                  </span>
+                </li>
+                <li className="meal_attribute">
+                  <div className="icon_inner">
+                    <IconRestaurant />
+                  </div>
+                  <span>
+                    {icon_inner_02}
+                    <strong>{icon_inner_strong_02}</strong>
+                  </span>
+                </li>
+                <li className="meal_attribute">
+                  <div className="icon_inner">
+                    <IconStar />
+                  </div>
+                  <span>
+                    <strong>{icon_inner_strong_03}</strong>
+                    {icon_inner_03}
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </CardStyles>
+        )
+      )}
     </>
   )
 }
@@ -90,8 +120,16 @@ const CardStyles = styled.div`
         background-color: #51cf66;
       }
 
+      .tag_vegan {
+        background-color: #94d82d;
+      }
+
       .tag_paleo {
         background-color: #ffd43b;
+      }
+
+      .none {
+        display: none;
       }
     }
 
@@ -114,14 +152,13 @@ const CardStyles = styled.div`
         align-items: center;
         gap: 1.6rem;
 
-        div {
+        .icon_inner {
           display: block;
           height: 2.4rem;
           width: 2.4rem;
           color: #e67e22;
 
           svg {
-            display: block;
             height: 100%;
             width: 100%;
             stroke: currentColor;
